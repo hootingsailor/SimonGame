46c58a327b165368a599cfa9428f02cdefb3ba84
Aakash Sharma <sharmaaakash8924@gmai.com>
2023-01-17 22:53:03 +0530

    some functions added

--- game.js
+++ game.js
@@ -1,12 +1,26 @@
 let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
+let userClickedPattern = [];
 
 function nextSequence(){
     let randomNumber = Math.floor((Math.random() * 4));
     let randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
     $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-    let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    playSound(randomChosenColour);
+}
+
+function playSound(name){
+    let audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
-nextSequence();
+
+function animatePress(currentColour){
+    
+}
+
+$(".btn").click(function(){
+    let userChosenColour = $(this).attr('id');
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+});
